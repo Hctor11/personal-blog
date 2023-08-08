13b9745ecf039b81d40aa57b1bf5993931823457
hector <hectorrrivera1010@gmail.com>
2023-08-08 01:25:49 -0600

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    markdown: {
+        remarkPlugins: [
+            remarkMath,
+        ],
+        rehypePlugins: [
+            [rehypeKatex]
+        ]
+    }
+});
