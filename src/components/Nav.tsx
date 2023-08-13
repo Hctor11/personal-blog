7dd177b28e4dbfb5b75963de614ef1cb68878c0a
hector <hectorrrivera1010@gmail.com>
2023-08-12 22:58:02 -0600

    [add] navigation bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+
+const Nav = () => {
+  return (
+    <div className="navigation">
+        <a href="/">
+            home
+        </a>
+        <a href="/about">
+            about me
+        </a>
+    </div>
+  )
+}
+
+export default Nav
