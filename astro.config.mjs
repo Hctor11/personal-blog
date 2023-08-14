72570ec41d18dd35ee3e07f8d2706a578746bfe0
hector <hectorrrivera1010@gmail.com>
2023-08-14 06:18:07 -0600

    [update] add initial deploy stage

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Hctor11.github.io',
+  base: '/personal-blog',
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
