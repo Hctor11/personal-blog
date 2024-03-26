ae1525b395b918537781c479dedfd4337dbf1a25
hector <hectorrrivera1010@gmail.com>
2024-03-25 23:16:32 -0600

    [add] entry 2 images

--- src/util/entries.ts
+++ src/util/entries.ts
@@ -8,7 +8,16 @@ const ENTRIES = [
     url: '/entries/1',
     img: '/entries/entry1.png',
     date: 'december 2023',
-  }
+  },
+  {
+    id: 2,
+    title: "El legado y el lenguaje en el internet.",
+    description:
+      "Agrupacion de pensamientos sobre el legado en tiempos de consumo masivo de contenido y los tipos de emisores en internet.",
+    url: '/entries/2',
+    img: '/entries/entry2.png',
+    date: 'march 2024',
+  },
 ];
 
 export default ENTRIES
